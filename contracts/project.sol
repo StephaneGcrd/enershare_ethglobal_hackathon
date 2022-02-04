474580d36734522dcd76987daea43b66e59802d6
Stephane Guichard <stephane@ownrs.dk>
2022-02-04 18:18:50 +0100

    edit contract

--- contracts/project.sol
+++ contracts/project.sol
@@ -1,19 +1,50 @@
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.10;
 
 contract ProjectToken {
 
-    uint256 totalSupply = 100;
-    uint256 circulatingSupply = 0;
+    uint256 public totalSupply = 1e19;
+    uint256 public circulatingSupply = 0;
     uint256 initialPricePerToken = 1;
 
     uint256 public treasurery; // 0 in the beginning, fills up with ICO
+    address owner;
 
     mapping(address => uint256) balances;
 
     event Transfer(address indexed _from, address indexed _to, uint _value);
 
+    /**
+     * @dev Set contract deployer as owner
+     */
+    constructor() {
+        owner = msg.sender;
+    }
 
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        // Return the balance for the specific address
+        return balances[_owner];
+    }
+
+    function buyICO(uint256 _amount) public payable {
+        require(_amount < totalSupply-circulatingSupply, "No more token available at that price");
+        require(_amount == msg.value, "No more token available at that price");
+        balances[msg.sender] = balances[msg.sender] + _amount;
+        circulatingSupply = circulatingSupply + _amount;
+        payable(owner).transfer(msg.value);
+    }
+
+    function addTreasurery() public payable {
+        treasurery += msg.value;
+    }
+
+    function claimTreasurery() public payable {
+        require(balances[msg.sender] > 0, "No tokens owned");
+        uint256 value = (balances[msg.sender]* treasurery) / totalSupply;
+        treasurery -= value;
+        payable(msg.sender).transfer(value);
+    }
+    //1000000000000000000
 
 }
